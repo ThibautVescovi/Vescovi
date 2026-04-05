50c176884f4eb760f675cecfa41892f37c0ec68b
ThibautVescovi <thibaut@vescovi.fr>
2026-04-05 11:18:04 +0200

    database

--- vescovi/app/page.tsx
+++ vescovi/app/page.tsx
@@ -1,3 +1,26 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabase'
+
 export default function Home() {
-    return <h1>Hello World</h1>
+    const [name, setName] = useState('Invité')
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const { data: users, error } = await supabase
+                .from('users')
+                .select('name')
+                .limit(1)
+
+            if (users && users.length > 0) {
+                setName(users[0].name)
+            } else {
+                console.log('Erreur Supabase:', error)
+            }
+        }
+        fetchUser()
+    }, [])
+
+    return <h1>Bonjour {name} 👋</h1>
 }
